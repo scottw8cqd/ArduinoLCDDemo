18b8a512f52956ba0e704d3489ef489cc3ac75b7
Scott Hernalsteen <scott@w8cqd.com>
2015-11-10 15:41:38 -0500

    Base code
    Base code for a quick Arduino I2C LCD Demo

---
+++ LCD_Test.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x3F,2, 1, 0, 4, 5, 6, 7, 3,POSITIVE); 
+
+int counter=0;
+
+void setup() {
+
+lcd.begin(16,2); 
+lcd.clear();
+lcd.print("Hello ITCS-2590!");
+lcd.setCursor(1,1);
+
+}
+
+void loop() {
+
+lcd.setCursor(7,1);
+counter++;
+lcd.print(counter);
+delay(1000);
+
+}
